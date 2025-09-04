78ecdf3c37f3883cb81762ab391b8293db26d26e
Diego Hernandez <dh8939693@gmail.com>
2025-09-04 01:14:28 -0400

    mas eliminaciones de archivos residuales

--- Hardware Tech/js/main.js
+++ Hardware Tech/js/main.js
@@ -292,72 +292,7 @@
     });
 
 
-    // Carrusel de vendedores
-    $('.vendor-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:3
-            },
-            768:{
-                items:4
-            },
-            992:{
-                items:5
-            },
-            1200:{
-                items:6
-            }
-        }
-    });
-
-
-    // Carrusel relacionado
-    $('.related-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            }
-        }
-    });
-
-
-    // Cantidad de producto
-    $('.quantity button').on('click', function () {
-        var button = $(this);
-        var oldValue = button.parent().parent().find('input').val();
-        if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        button.parent().parent().find('input').val(newVal);
-    });
+    // Código de carruseles y control de cantidad eliminado por no utilizarse tras retirar detail.html y secciones relacionadas.
     
 })(jQuery);
 
